dbc5a8eec9f45293baf3e5f22652fccaa10dbae0
liujiwei <liujiwei0529@163.com>
2018-07-18 17:50:50 +0800

    创建springboot项目

---
+++ src/main/java/net/abadguy/smallprogreamweb/SmallprogreamwebApplication.java
@@ -0,0 +1,12 @@
+package net.abadguy.smallprogreamweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallprogreamwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallprogreamwebApplication.class, args);
+    }
+}
---
+++ src/test/java/net/abadguy/smallprogreamweb/SmallprogreamwebApplicationTests.java
@@ -0,0 +1,16 @@
+package net.abadguy.smallprogreamweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmallprogreamwebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
