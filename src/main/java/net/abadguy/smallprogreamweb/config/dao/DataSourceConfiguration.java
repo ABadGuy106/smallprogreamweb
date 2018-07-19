0b2404d1d2f2714d1c5549e5a4e9b97d3cbeb9f9
liujiwei <liujiwei0529@163.com>
2018-07-19 09:57:41 +0800

    创建bean,写入数据库连接配置

---
+++ src/main/java/net/abadguy/smallprogreamweb/config/dao/DataSourceConfiguration.java
@@ -0,0 +1,31 @@
+package net.abadguy.smallprogreamweb.config.dao;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.PropertyVetoException;
+
+public class DataSourceConfiguration {
+    @Value("${jdbc.driver}")
+    private String jdbcDriver;
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${jdbc.username}")
+    private String jdbcUser;
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
+
+    @Bean(name="dataSource")
+    public ComboPooledDataSource createDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource=new ComboPooledDataSource();
+
+        dataSource.setDriverClass(jdbcDriver);
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUser(jdbcUser);
+        dataSource.setPassword(jdbcPassword);
+        //关闭连接后不自动提交
+        dataSource.setAutoCommitOnClose(false);
+        return dataSource;
+    }
+}
